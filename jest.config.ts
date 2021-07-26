d2a7613c37b290351a9f7d0355ff56c8d7e3d9c0
Igor Carlos <igor.carlos@gerencianet.com.br>
2021-07-26 14:18:51 -0300

    refactor: add folders to ignore in jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ export default {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  modulePathIgnorePatterns: ['protocols', 'helpers', 'errors'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   transform: { '.+\\.ts$': 'ts-jest' }
