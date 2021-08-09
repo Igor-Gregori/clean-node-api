709a53abf2928fda128a41058be9bdccc310b8cc
Igor Gregori <igsilvacarlos@gmail.com>
2021-08-09 01:13:46 -0300

    refactor: move all signup protocols to a single file

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ export default {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  modulePathIgnorePatterns: ['protocols', 'helpers', 'errors'],
+  modulePathIgnorePatterns: ['protocols', 'helpers', 'errors', 'domain'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   transform: { '.+\\.ts$': 'ts-jest' }
