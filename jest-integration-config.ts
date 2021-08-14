141262d2cf7847270c96845fd48ba581174ec827
Igor Gregori <igsilvacarlos@gmail.com>
2021-08-14 18:16:38 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.ts
@@ -0,0 +1,4 @@
+import config from './jest.config'
+// @ts-expect-error
+config.testMatch = ['**/*.test.ts']
+export default config
---
+++ jest-unit-config.ts
@@ -0,0 +1,4 @@
+import config from './jest.config'
+// @ts-expect-error
+config.testMatch = ['**/*.spec.ts']
+export default config
