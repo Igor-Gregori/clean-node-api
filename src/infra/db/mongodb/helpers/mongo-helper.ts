2f2451ade1d48c15ba70fcbfb2ecd85f87c67721
Igor <igor.carlos@alunos.unis.edu.br>
2021-11-15 19:08:05 -0300

    feat: ensure AccountMongoRepository retuns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,14 +1,14 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: MongoClient,
   async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useNewUnifiedTopology: true
-    })
+    this.client = await MongoClient.connect(url)
   },
   async disconnect(): Promise<void> {
     await this.client.close()
+  },
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
